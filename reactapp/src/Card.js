f669a783cfa7685d37cb054e227202c23436b5d1
Iamneo <techadmin@iamneo.ai>
2023-07-31 15:59:10 +0000

    Backup Commit

--- reactapp/src/Card.js
+++ reactapp/src/Card.js
@@ -1,8 +1,22 @@
 import React, {useState } from 'react';
 import Button from '../Button/Button';
-const Card = ({key, question, correctAnswerMarkUpdate, attempt, options, answer}) => {const[state, setState] = useState(false);
-function disableBtn()
+const Card = ({key, question, correctAnswerMarkUpdate, attempt, options, answer}) => 
 {
-    setState(true);
-}
+    const[state, setState] = useState(false);
+    function disableBtn()
+    {
+        setState(true);
+    }
+    return(
+        <div>
+        <h4>
+            {question}
+        </h4>
+        <Button onClick = {disableBtn} disabled = {state}> {options.option1} </Button>
+        <Button onClick = {disableBtn} disabled = {state}> {options.option2} </Button>
+        <Button onClick = {disableBtn} disabled = {state}> {options.option3} </Button>
+        <Button onClick = {disableBtn} disabled = {state}> {options.option4} </Button>
+        </div>
+    )
+
 }
