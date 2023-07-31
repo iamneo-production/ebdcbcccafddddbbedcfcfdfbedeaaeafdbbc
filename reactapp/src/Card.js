6a63010cd6c3b9ae146b0ee02bab3d4c1cbd070c
Iamneo <techadmin@iamneo.ai>
2023-07-31 15:54:11 +0000

    Backup Commit

--- reactapp/src/Card.js
+++ reactapp/src/Card.js
@@ -1,1 +1,8 @@
-import 
+import React, {useState } from 'react';
+import Button from '../Button/Button';
+const Card = ({key, question, correctAnswerMarkUpdate, attempt, options, answer}) => {const[state, setState] = useState(false);
+function disableBtn()
+{
+    setState(true);
+}
+}
