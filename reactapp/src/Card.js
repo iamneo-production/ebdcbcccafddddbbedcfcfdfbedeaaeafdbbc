deac620a143627043eceb2f3aedca0e80466db13
Iamneo <techadmin@iamneo.ai>
2023-08-03 06:31:51 +0000

    Backup Commit

--- reactapp/src/Card.js
+++ reactapp/src/Card.js
@@ -1,31 +1,22 @@
-Card.js
-
-import React, { useState } from 'react';
+import React, {useState } from 'react';
 import Button from '../Button/Button';
-const Card = ({key,
-    question,
-    correctAnswerMarkUpdate,
-    attempt,
-    options,
-    answer}) => {
-        const [state, setSate] = useState(false);
-        function disableBtn()
-        {
-            setSate(true);
-        }
-
-
-        return (
-            <div>
-            <h4>
-                {question}
-            </h4>
-            <Button onClick={disableBtn} disabled = {state} >{options.option1}</Button>
-            <Button onClick={disableBtn} disabled = {state} >{options.option2}</Button>
-            <Button onClick={disableBtn} disabled = {state} >{options.option3}</Button>
-            <Button onClick={disableBtn} disabled = {state} >{options.option4}</Button>
-            </div>
-        )
+const Card = ({key, question, correctAnswerMarkUpdate, attempt, options, answer}) => 
+{
+    const[state, setState] = useState(false);
+    function disableBtn()
+    {
+        setState(true);
     }
-
-    export default Card;
+    return(
+        <div>
+        <h4>
+            {question}
+        </h4>
+        <Button onClick = {disableBtn} disabled = {state}> {options.option1} </Button>
+        <Button onClick = {disableBtn} disabled = {state}> {options.option2} </Button>
+        <Button onClick = {disableBtn} disabled = {state}> {options.option3} </Button>
+        <Button onClick = {disableBtn} disabled = {state}> {options.option4} </Button>
+        </div>
+    )
+}
+export default Card;
